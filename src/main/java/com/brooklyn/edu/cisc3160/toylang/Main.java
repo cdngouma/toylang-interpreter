6d4c1bcfe56be10fe9f76e390f8bfd7bf8b04fa9
cdngouma <ngouma.chrys@gmail.com>
2019-12-01 02:03:35 -0500

    Implemented option to load program from file

--- src/main/java/com/brooklyn/edu/cisc3160/toylang/Main.java
+++ src/main/java/com/brooklyn/edu/cisc3160/toylang/Main.java
@@ -4,27 +4,35 @@ import com.brooklyn.edu.cisc3160.toylang.interpreter.Interpreter;
 
 import java.io.*;
 import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         try {
+            String fileName;
             String program;
             if (args.length < 2) {
-                System.out.println("Info: Not enough arguments entered.\nexecuting built-in default test program...");
-                // Default test program
-                program = "x=1;\ny = 2;\nz = ---(x*y)*(x+-y);";
-            } else {
-                if (args[0].compareTo("--vars") == 0) {
-                    // locate file
-                    String fileName = args[1];
-                    program = loadProgram(fileName);
+                System.out.println("Info: Not enough arguments entered.\nloading default test program...");
+                fileName = "test.toy";
+                ClassLoader classLoader = Main.class.getClassLoader();
+                File file = new File(Objects.requireNonNull(classLoader.getResource(fileName)).getFile());
+                program = loadProgram(file);
+            } else if (args[0].compareTo("--vars") == 0) {
+                fileName = args[1];
+                if (!Pattern.matches(".toy$", fileName)) {
+                    throw new Exception("Error: unsupported file format.");
                 } else {
-                    throw new Exception(String.format("Command unknown: '%s'", args[0]));
+                    // TODO: Implement functionality to load files
+                    throw new Exception("Info: External files not supported yet.");
                 }
+            } else {
+                throw new Exception(String.format("Command unknown: '%s'.", args[0]));
             }
 
             Interpreter interpreter = new Interpreter();
-            Map<String,Integer> varsTable = interpreter.getVarsTable(program);
+            Map<String, Integer> varsTable = interpreter.getVarsTable(program);
+
             for (String var : varsTable.keySet()) {
                 System.out.println(String.format("%s = %d", var, varsTable.get(var)));
             }
@@ -34,20 +42,18 @@ public class Main {
         }
     }
 
-    private static String loadProgram(String fileName) {
+    private static String loadProgram(File file) {
         StringBuilder sb = new StringBuilder();
-        File file = new File(fileName + ".toy");
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file));){
+        try (BufferedReader reader = new BufferedReader(new FileReader(file));) {
             String text = null;
             while ((text = reader.readLine()) != null) {
-                sb.append(Integer.parseInt(text));
+                sb.append(text);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println(sb.toString());
         return sb.toString();
     }
 }
