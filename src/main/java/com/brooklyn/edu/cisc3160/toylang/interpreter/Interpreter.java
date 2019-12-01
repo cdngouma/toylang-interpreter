35a5e0287aa0b34b165cb1e105f998c8cd172cea
cdngouma <ngouma.chrys@gmail.com>
2019-12-01 01:31:56 -0500

    Documented Interpreter.java

--- src/main/java/com/brooklyn/edu/cisc3160/toylang/interpreter/Interpreter.java
+++ src/main/java/com/brooklyn/edu/cisc3160/toylang/interpreter/Interpreter.java
@@ -11,26 +11,27 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Interpreter {
-
-    public static void run(String program) {
-        try {
-            Parser parser = new Parser();
-            List<String> tokens = Tokenizer.tokenize(program);
-            printVarsValues(parser.parse(tokens));
-        } catch (UnexpectedTokenException | UninitializedVariableException e) {
-            System.err.println(e.getMessage());
-        }
+    /**
+     * Generates a variable table from a given program source code.
+     * @param program A string representation of the program source code.
+     * @return The variable table generated from a given program source code.
+     * @throws UnexpectedTokenException When the program does not follow the language CFS.
+     * @throws UninitializedVariableException When an uninitialized variable is detected.
+     */
+    public Map<String, Integer> getVarsTable(String program) throws UnexpectedTokenException, UninitializedVariableException {
+        Parser parser = new Parser();
+        List<String> tokens = Tokenizer.tokenize(program);
+        return buildVarsTable(parser.parse(tokens));
     }
 
-    private static void printVarsValues(ParseTree tree) throws UninitializedVariableException {
-        Map<String,Integer> varsTable = buildVarsTable(tree);
-        for (String var : varsTable.keySet()) {
-            System.out.println(String.format("%s = %d", var, varsTable.get(var)));
-        }
-    }
-
-    private static Map<String,Integer> buildVarsTable(ParseTree parseTree) throws UninitializedVariableException {
-        Map<String,Integer> varsTable = new HashMap<>();
+    /**
+     * Build a variable table from a Parse Tree.
+     * @param parseTree The Parse Tree generated from the given program source code.
+     * @return A variable table.
+     * @throws UninitializedVariableException
+     */
+    private Map<String, Integer> buildVarsTable(ParseTree parseTree) throws UninitializedVariableException {
+        Map<String, Integer> varsTable = new HashMap<>();
         for (ASTNode node : parseTree.getChildren()) {
             String var = node.getLeft().getValue();
             int value = evaluateAST(node.getRight(), varsTable);
@@ -39,9 +40,15 @@ public class Interpreter {
         return varsTable;
     }
 
-    private static int evaluateAST(ASTNode node, Map<String, Integer> varsTable) throws UninitializedVariableException {
+    /**
+     * Evaluate each branch of the AST.
+     * @param node An Abstract Syntax Tree Node.
+     * @param varsTable A variable table.
+     * @return The value obtained from evaluating a branch of the AST.
+     * @throws UninitializedVariableException When an uninitialized variable is detected.
+     */
+    private int evaluateAST(ASTNode node, Map<String, Integer> varsTable) throws UninitializedVariableException {
         if (node == null) return 1;
-
         int leftVal = evaluateAST(node.getLeft(), varsTable);
         int rightVal = evaluateAST(node.getRight(), varsTable);
 
