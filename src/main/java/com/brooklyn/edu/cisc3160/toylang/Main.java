b31fa9e70492986ca3af878ceda20c6592ba31c9
cdngouma <ngouma.chrys@gmail.com>
2020-08-30 15:10:12 -0400

    read source code from file or write it

--- src/main/java/com/brooklyn/edu/cisc3160/toylang/Main.java
+++ src/main/java/com/brooklyn/edu/cisc3160/toylang/Main.java
@@ -1,35 +1,68 @@
 package com.brooklyn.edu.cisc3160.toylang;
 
 import com.brooklyn.edu.cisc3160.toylang.interpreter.Interpreter;
+import com.brooklyn.edu.cisc3160.toylang.interpreter.exception.UnexpectedTokenException;
+import com.brooklyn.edu.cisc3160.toylang.interpreter.exception.UninitializedVariableException;
 
 import java.io.*;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            if (args.length >= 2) {
-                String path = args[1];
-
-                if (args[0].compareTo("-vars") == 0 || args[0].compareTo("--variables") == 0) {
-                    String srcCode = extractSourceCode(path);
-                    Interpreter interpreter = new Interpreter();
-                    Map<String, Integer> varsTable = interpreter.getVarsTable(srcCode);
-
-                    System.out.println("printing variables...");
-                    for (String var : varsTable.keySet()) {
-                        System.out.println(String.format("%s = %d", var, varsTable.get(var)));
+            if (args.length > 0) {
+                final String arg = args[0];
+                if (arg.compareTo("-vars") == 0 || arg.compareTo("--vars-table") == 0) {
+                    String srcCode = "";
+                    if (args.length == 2) {
+                        final String path = args[1];
+                        srcCode = getSourceCodeFromFile(path);
+                    } else {
+                        srcCode = writeSourceCode();
                     }
+                    printVarsTable(srcCode);
                 } else {
-                    throw new Exception(String.format("Command unknown: '%s'.", args[0]));
+                    throw new Exception(String.format("Command unknown: '%s'.", args[0].replaceFirst("^(-|--)", "")));
                 }
             } else {
-                throw new Exception("Not enough arguments");
+                System.out.println("No arguments entered\nRunning default script...\ntest.toy");
+                String path = "src\\main\\resources\\test.toy";
+                String srcCode = getSourceCodeFromFile(path);
+                System.out.println(srcCode);
+                printVarsTable(srcCode);
             }
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    private static void printVarsTable(String srcCode) throws UnexpectedTokenException, UninitializedVariableException {
+        Interpreter interpreter = new Interpreter();
+        Map<String, Integer> varsTable = interpreter.getVarsTable(srcCode);
+        // Print variables and their values
+        System.out.println("printing variables table...");
+        for (String var : varsTable.keySet()) {
+            System.out.println(String.format("%s = %d", var, varsTable.get(var)));
+        }
+    }
+
+    /**
+     * Register user input as source code.
+     * @return String representation of the source code.
+     */
+    private static String writeSourceCode() {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        System.out.println("Type '$END' then press <ENTER> to finish");
+        line = scanner.nextLine();
+        while (line.compareTo("$END") != 0) {
+            sb.append(line);
+            line = scanner.nextLine();
+        }
+        return sb.toString();
     }
 
     /**
@@ -38,13 +71,13 @@ public class Main {
      * @return String representation of the source code.
      * @throws Exception When a supported file could not be found.
      */
-    private static String extractSourceCode(String path) throws Exception {
+    private static String getSourceCodeFromFile(String path) throws Exception {
         if (!Pattern.matches("^\\S+\\.toy$", path)) throw new Exception("File not supported");
         File file = new File(path);
 
         StringBuilder sb = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(file));) {
-            String text = null;
+            String text;
             while ((text = reader.readLine()) != null) {
                 sb.append(text);
             }
